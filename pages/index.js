d342ce14dddad4c67e698bb451ec551538deda13
MaxTorras <max13torras@gmail.com>
2025-10-01 12:55:42 +0100

    Change home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-// pages/index.js
 export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to the Attendance App 🎉</h1>
-      <p><a href="/qr">Go to QR Code</a></p>
-      <p><a href="/check-in">Go to Check-In</a></p>
-    </div>
-  );
+  return <h1>Welcome to the Attendance App 🚀</h1>;
 }
